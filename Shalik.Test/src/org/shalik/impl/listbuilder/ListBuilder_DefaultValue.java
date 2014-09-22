a66da1c6f04dbabe875824fef79551dbbb2dc4db
Mohayemin <Mohayemin@localhost>
2014-09-22 21:55:04 +0600

    Test class refactored

--- Shalik.Test/src/org/shalik/impl/listbuilder/ListBuilder_DefaultValue.java
+++ Shalik.Test/src/org/shalik/impl/listbuilder/ListBuilder_DefaultValue.java
@@ -2,8 +2,6 @@ package org.shalik.impl.listbuilder;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.shalik.impl.ListBuilderImpl;
@@ -12,56 +10,55 @@ import org.shalik.test.testdoubles.AllTypes;
 public class ListBuilder_DefaultValue {
 
 	private ListBuilderImpl<AllTypes> builder;
-	private List<AllTypes> objects;
-
+	private AllTypes object0;
 	@Before
 	public void setupForEach() {
 		builder = new ListBuilderImpl<>(AllTypes.class);
-		objects = builder.build(10);
+		object0 = builder.build(10).get(0);
 	}
 
 	@Test
 	public void build_10_sizeIsTen() {
-		assertEquals(10, objects.size());
+		assertEquals(10, builder.build(10).size());
 	}
 
 	@Test
 	public void build_firstShortIs0() {
-		assertEquals(0, objects.get(0).getShortInteger());
+		assertEquals(0, object0.getShortInteger());
 	}
 	
 	@Test
 	public void build_firstIntIs0() {
-		assertEquals(0, objects.get(0).getInterger());
+		assertEquals(0, object0.getInterger());
 	}
 	
 	@Test
 	public void build_firstLongIs0() {
-		assertEquals(0, objects.get(0).getLongInteger());
+		assertEquals(0, object0.getLongInteger());
 	}
 	
 	@Test
 	public void build_firstFloatIs0f() {
-		assertEquals(0.f, objects.get(0).getSingleFloat(), 1e-10);
+		assertEquals(0.f, object0.getSingleFloat(), 1e-10);
 	}
 	
 	@Test
 	public void build_firstDoubleIs0() {
-		assertEquals(0., objects.get(0).getDoubleFloat(), 1e-10);
+		assertEquals(0., object0.getDoubleFloat(), 1e-10);
 	}
 	
 	@Test
 	public void build_firstCharIsUpperCaseA() {
-		assertEquals('A', objects.get(0).getCharacter());
+		assertEquals('A', object0.getCharacter());
 	}
 	
 	@Test
 	public void build_firstBoolIsFalse() {
-		assertEquals(false, objects.get(0).isBool());
+		assertEquals(false, object0.isBool());
 	}
 	
 	@Test
 	public void build_firstStringIs_string0() {
-		assertEquals("string0", objects.get(0).getString());
+		assertEquals("string0", object0.getString());
 	}
 }
