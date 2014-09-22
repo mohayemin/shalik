8a2d05a70f438689047f20b77b0f8a037fac7348
Mohayemin <Mohayemin@localhost>
2014-09-22 22:23:09 +0600

    test methods renamed

--- Shalik.Test/src/org/shalik/impl/listbuilder/ListBuilder_Increament.java
+++ Shalik.Test/src/org/shalik/impl/listbuilder/ListBuilder_Increament.java
@@ -18,32 +18,32 @@ public class ListBuilder_Increament {
 	}
 	
 	@Test
-	public void build_get5getShort_5() {
+	public void build_short5_5() {
 		assertEquals(5, object5.getShortInteger());
 	}
 	
 	@Test
-	public void build_get5getInt_5() {
+	public void build_int5_5() {
 		assertEquals(5, object5.getInterger());
 	}
 	
 	@Test
-	public void build_get5getLong_5() {
+	public void build_long5_5() {
 		assertEquals(5L, object5.getLongInteger());
 	}
 	
 	@Test
-	public void build_get5getFloat_5() {
+	public void build_float5_5() {
 		assertEquals(5.f, object5.getSingleFloat(), 1e-10);
 	}
 	
 	@Test
-	public void build_get5getDouble_5() {
+	public void build_double5_5() {
 		assertEquals(5., object5.getDoubleFloat(), 1e-10);
 	}
 		
 	@Test
-	public void build_get5getString_string5() {
+	public void build_string5_string5() {
 		assertEquals("string5", object5.getString());
 	}
 }
